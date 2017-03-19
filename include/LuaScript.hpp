7e9011027b718403cfbc895606fe3b07ec482226
blazgrom <slavi.angelov94@gmail.com>
2017-03-19 21:31:00 +0100

    Added set functionality for all the basic type(string,int,double,bool). The get function is now getAs seen that the user has to specify the type of the return value

--- include/LuaScript.hpp
+++ include/LuaScript.hpp
@@ -19,16 +19,21 @@ public:
 			throw std::exception(error_message.c_str());
 		}
 	}
-	//Gets
 	template <typename T >
-	T get(const std::string& variableName)
+	//Gets a global variable from the file loaded during the creation of the object
+	T getAs(const std::string& variableName)
 	{
 		lua_getglobal(_lState, variableName.c_str());
 		auto value = getValue(T());
 		return static_cast<T> (value);
 	}
-
-	//Sets
+	template <typename T >
+	//Sets a global variable in the file loaded during the creation of the object
+	void set(const std::string& variableName,const T& value)
+	{
+		setValue(value);
+		lua_setglobal(_lState, variableName.c_str());
+	}
 	~LuaScript()
 	{
 		lua_close(_lState);
@@ -36,6 +41,7 @@ public:
 
 private:
 	lua_State* _lState;
+	//Get implementations
 	lua_Number getValue(double)
 	{
 		return  lua_tonumber(_lState, -1);
@@ -51,6 +57,23 @@ private:
 	bool getValue(bool)
 	{
 		return lua_toboolean(_lState, -1);
+	}
+	//Set implementations
+	void setValue(const bool val)
+	{
+		 lua_pushboolean(_lState, val);
+	}
+	void setValue(const std::string& val)
+	{
+		lua_pushlstring(_lState, val.c_str(),val.size());
+	}
+	void setValue(const double val)
+	{
+		lua_pushnumber(_lState,val);
+	}
+	void setValue(const int val)
+	{
+		lua_pushinteger(_lState, val);
 	}
 };
 #endif // !LUA_SCRIPT_HPP
