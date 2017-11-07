db05c4ba999b357acd23cf969cf47658d3f01fed
Slavi <slavi.angelov94@gmail.com>
2017-11-07 20:27:49 +0100

    Added some simple test for the 'get' functionality

--- src/core/tests/LuaScript_Test.cpp
+++ src/core/tests/LuaScript_Test.cpp
@@ -13,10 +13,9 @@ public:
     string script_folder_path;
     void SetUp() override
     {
-        script_folder_path="..//src//core//tests//";
+        script_folder_path="../src/core/tests/";
         main_script_dep=script_folder_path+"luascript_dep.lua";
         main_script_dep2=script_folder_path+"luascript_dep2.lua";
-        
         main_script=script_folder_path+"luascript_test.lua";
     }
 };
@@ -35,3 +34,31 @@ TEST_F(LuaScriptF,LoadingMultipleDependenciesAndMainScript)
     ASSERT_TRUE(first_dep==script.get<string>("dependency"));
     ASSERT_TRUE(second_dep==script.get<string>("dependency2"));
 }
+TEST_F(LuaScriptF,GetVariableBool)
+{
+    LuaScript script(main_script);
+    ASSERT_TRUE(script.get<bool>("boolt_var"));
+    ASSERT_FALSE(script.get<bool>("boolf_var"));
+}
+TEST_F(LuaScriptF,GetVariableString)
+{
+    LuaScript script(main_script);
+    string expected="This is some string";
+    ASSERT_TRUE(expected==script.get<string>("string_var"));
+}
+TEST_F(LuaScriptF,GetVariableInteger)
+{
+    LuaScript script(main_script);
+    auto a=script.get<int>("integer_var");
+    ASSERT_EQ(1,script.get<int>("integer_var"));
+}
+// TEST_F(LuaScriptF,GetVariableDouble)
+// {
+//     LuaScript script(main_script);
+//     ASSERT_DOUBLE_EQ(102.0351,script.get<double>("double_var"));
+// }
+// TEST_F(LuaScriptF,GetVariableFloat)
+// {
+//     LuaScript script(main_script);
+//     ASSERT_FLOAT_EQ(88.15836,script.get<float>("float_var"));
+// }
---
+++ src/core/tests/luascript_test.lua
@@ -0,0 +1,7 @@
+--Get testing
+boolt_var=true;
+boolf_var=false;
+string_var="This is some string";
+integer_var=100;
+double_var=102.0351;
+float_var=88.15836;
