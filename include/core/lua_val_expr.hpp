577a357f0751d3e302e9c5f477bdd89fe5ead51d
Slavi <slavi.angelov94@gmail.com>
2017-11-25 18:21:59 +0100

    Added copy assignment operator and some helper functions

--- include/core/lua_val_expr.hpp
+++ include/core/lua_val_expr.hpp
@@ -5,13 +5,15 @@
 namespace LuaBz
 {
 class lua_script;
-friend class lua_script;
 namespace experimental
 {
 class lua_value
 {
+    friend class lua_script;
+
   public:
     ~lua_value() = default;
+    lua_value &operator=(const lua_value &rhs);
     operator long long() const;
     operator unsigned long long() const;
     operator long() const;
@@ -25,6 +27,7 @@ class lua_value
     operator bool() const;
     operator char() const;
     operator std::string() const;
+
   private:
     lua_value(lua_State *state, const std::string &name);
     lua_value(const lua_value &rhs);
@@ -37,19 +40,35 @@ class lua_value
      * get_value The function that will be used to retrieve the value from the
      * stack \returns The lua variables identified by m_name transformed into
      * type T
+     * \todo Decide if it makes sense to be const
      */
-    template <typename T>
-    T value_as(void (*get_value)(lua_State *state, int index))
+    template <typename T, typename OriginalType>
+    T value_as(OriginalType (*get_value)(lua_State *state, int index)) const
     {
-        lua_getglobal(m_state, m_name);
+        lua_getglobal(m_state, m_name.c_str());
         return static_cast<T>(get_value(m_state, top));
     }
+    /**
+     * \brief Retrieves the value of the lua variable
+     *
+     * Retrieves the value of the lua variable identified by m_name, thus
+     * pushing it on top of the stack identified by m_state
+     */
+    void get_lua_var() const;
+    /**
+     * \brief Sets the lua variable to a new value
+     *
+     * Sets the value of the variable identified by m_name.
+     * \pre The new value is already be on top of the lua stack 
+     * (m_state in this case).
+     */
+    void set_lua_var();
     lua_State *m_state;  ///< A lua state which represents the file with which
                          ///< the script was opened
     const std::string m_name;  ///< The name of the lua variable which this
                                ///< lua_value object represents
-    static const int
-        top;  ///< A simple identifier for the top element of the stack
+    static const int top;  ///< A simple identifier for the top element of the
+                           ///< stack
 };
 }
 }
--- src/core/lua_val_expr.cpp
+++ src/core/lua_val_expr.cpp
@@ -3,15 +3,52 @@ namespace LuaBz
 {
 namespace experimental
 {
-static int lua_value::top = -1;
+const int lua_value::top = -1;
 lua_value::lua_value(lua_State *state, const std::string &name)
   : m_state{state}, m_name{name}
 {
 }
 
 lua_value::lua_value(const lua_value &rhs)
-  : m_state{rhs.m_state}, m_name{rhs.m_state}
+  : m_state{rhs.m_state}, m_name{rhs.m_name}
 {
+}
+/**
+ * \details The copy operation uses the following logic:
+ * If the variables use the same lua state we simple do getglobal setglobal in
+ * order to copy the values. If the values are on two different states, we take
+ * the value of rhs and push it into this->m_state and then we call setglobal
+ * all while retaining the type of the value from rhs. \todo Handle the case
+ * when rhs element is a table
+ */
+lua_value &lua_value::operator=(const lua_value &rhs)
+{
+    rhs.get_lua_var();
+    if (m_state == rhs.m_state) {
+        set_lua_var();
+    } else {
+        switch (lua_type(rhs.m_state, top)) {
+            case LUA_TNIL:
+                lua_pushnil(m_state);
+                break;
+            case LUA_TBOOLEAN:
+                lua_pushboolean(m_state, lua_toboolean(rhs.m_state, top));
+                break;
+            case LUA_TNUMBER:
+                lua_pushnumber(m_state, lua_tonumber(rhs.m_state, top));
+                break;
+            case LUA_TSTRING:
+                lua_pushstring(m_state, lua_tostring(rhs.m_state, top));
+                break;
+            case LUA_TTABLE:
+                break;
+            case LUA_TFUNCTION:
+                lua_pushcfunction(m_state, lua_tocfunction(rhs.m_state, top));
+                break;
+        }
+        set_lua_var();
+    }
+    return *this;
 }
 lua_value::operator long long() const
 {
@@ -55,18 +92,26 @@ lua_value::operator double() const
 }
 lua_value::operator bool() const
 {
-    lua_getglobal(m_state, m_name);
-    lua_toboolean(m_state, top);
+    get_lua_var();
+    return lua_toboolean(m_state, top);
 }
 lua_value::operator std::string() const
 {
-    lua_getglobal(m_state, m_name);
-    lua_tostring(m_state, top);
+    get_lua_var();
+    return lua_tostring(m_state, top);
 }
 lua_value::operator char() const
 {
-    lua_getglobal(m_state, m_name);
-    (lua_tostring(m_state, top))[0];
+    get_lua_var();
+    return (lua_tostring(m_state, top))[0];
+}
+void lua_value::get_lua_var() const
+{
+    lua_getglobal(m_state, m_name.c_str());
+}
+void lua_value::set_lua_var()
+{
+    lua_setglobal(m_state, m_name.c_str());
 }
 }
 }
