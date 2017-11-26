3107b152cbe41fbc40893ff5c8c850bb78644c8f
Slavi <slavi.angelov94@gmail.com>
2017-11-26 22:04:28 +0100

    The test now use the new structure, also disabled some of the test because i'm not yet sure how i want to implement those functionalities. Add some comments for future test ideas.

--- tests/core/lua_script_test.cpp
+++ tests/core/lua_script_test.cpp
@@ -18,7 +18,7 @@ class lua_scriptF : public Test
     LuaBz::lua_script script;
     void SetUp() override
     {
-        script_folder_path = "tests/core/";
+        script_folder_path = "../tests/core/";
         main_script_dep = script_folder_path + "luascript_dep.lua";
         main_script_dep2 = script_folder_path + "luascript_dep2.lua";
         main_script = script_folder_path + "luascript_test.lua";
@@ -28,58 +28,64 @@ class lua_scriptF : public Test
         script.open(main_script, dependencies);
     }
 };
-TEST_F(lua_scriptF, TestingOperatorSquareBrackets)
-{
-    script["integer_var"];
-    ASSERT_TRUE(true);
-}
-TEST_F(lua_scriptF, LoadingDependencyAndMainScript)
-{
-    ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
-}
-TEST_F(lua_scriptF, LoadingMultipleDependenciesAndMainScript)
-{
-    ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
-    ASSERT_TRUE(second_dependency_string == script.get<string>("dependency2"));
-}
+// TEST_F(lua_scriptF, LoadingDependencyAndMainScript)
+// {
+//     ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
+// }
+// TEST_F(lua_scriptF, LoadingMultipleDependenciesAndMainScript)
+// {
+//     ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
+//     ASSERT_TRUE(second_dependency_string == script.get<string>("dependency2"));
+// }
+///Test ideas
+///1 SetLuaIntegerWithX where x is any integral type
+///2 SetLuaNumberWithX where x is any integral type
+///3 SetLuaBoolWithX where x is anything that can be converted to bool
+
 TEST_F(lua_scriptF, GetLuaBool)
 {
-    ASSERT_TRUE(script.get<bool>("boolt_var"));
-    ASSERT_FALSE(script.get<bool>("boolf_var"));
+    ASSERT_TRUE(script["boolt_var"]);
+    ASSERT_FALSE(script["boolf_var"]);
 }
 TEST_F(lua_scriptF, GetLuaString)
 {
     string expected = "This is some string";
-    ASSERT_TRUE(expected == script.get<string>("string_var"));
+    std::string actual=script["string_var"];
+    ASSERT_TRUE(expected == actual);
 }
 TEST_F(lua_scriptF, GetLuaInteger)
 {
-    ASSERT_EQ(100, script.get<long>("integer_var"));
+    int integer_var=script["integer_var"];
+    ASSERT_EQ(100,integer_var);
 }
 TEST_F(lua_scriptF, GetLuaNumber)
 {
-    ASSERT_DOUBLE_EQ(102.0351, script.get<double>("double_var"));
+    double double_var=script["double_var"];
+    ASSERT_DOUBLE_EQ(102.0351, double_var);
 }
 TEST_F(lua_scriptF, SetLuaBool)
 {
-    script.set("boolt_var", false);
-    ASSERT_FALSE(script.get<bool>("boolt_var"));
+    script["boolt_var"]=false;
+    ASSERT_FALSE(script["boolt_var"]);
 }
 TEST_F(lua_scriptF, SetLuaString)
 {
     std::string new_value = "This is the new value for the lua string";
-    script.set("string_var", new_value);
-    ASSERT_TRUE(new_value == script.get<string>("string_var"));
+    script["string_var"]=new_value;
+    std::string actual=script["string_var"];
+    ASSERT_TRUE(new_value == actual);
 }
 TEST_F(lua_scriptF, SetLuaInteger)
 {
-    script.set("integer_var", 666);
-    ASSERT_EQ(666, script.get<int>("integer_var"));
+    script["integer_var"]=666;
+    int integer_var=script["integer_var"];
+    ASSERT_EQ(666, integer_var);
 }
 TEST_F(lua_scriptF, SetLuaNumber)
 {
-    script.set("double_var", 10.132265);
-    ASSERT_DOUBLE_EQ(10.132265f, script.get<float>("double_var"));
+    script["double_var"]=10.132265;
+    double double_var=script["double_var"];
+    ASSERT_DOUBLE_EQ(10.132265, double_var);
 }
 TEST_F(lua_scriptF, ExecuteLuaCode)
 {
@@ -89,14 +95,15 @@ TEST_F(lua_scriptF, ExecuteLuaCode)
                   R"(    return input;)"
                   R"(end )"
                   R"(variable=increment(variable) )";
-    script.run(code);
-    ASSERT_EQ(2, script.get<long>("variable"));
+    script(code);
+    int variable=script["variable"];
+    ASSERT_EQ(2, variable);
 }
 
-TEST_F(lua_scriptF, GetLuaTable)
-{
-    lua_table position = script.get<lua_table>("Position");
-    int expected = 1;
-    int actual = position["x"];
-    ASSERT_EQ(expected, actual);
-}
+// TEST_F(lua_scriptF, GetLuaTable)
+// {
+//     lua_table position = script.get<lua_table>("Position");
+//     int expected = 1;
+//     int actual = position["x"];
+//     ASSERT_EQ(expected, actual);
+// }
