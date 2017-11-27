7ed8c9a153b5dc8a901577ca928d0f5cb2061832
Slavi <slavi.angelov94@gmail.com>
2017-11-27 21:26:28 +0100

    Added two new files, each testing different thing.. as it should be

---
+++ tests/core/lua_value_GetTest.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include "core/lua_script.hpp"
+
+class lua_value_Get: public ::testing::Test
+{
+  public:
+    LuaBz::lua_script script;
+    void SetUp() override
+    {
+        std::string lua_script_file = "../tests/core/luascript_test.lua";
+        script.open(lua_script_file);
+    }
+};
+TEST_F(lua_value_Get, LuaBoolAsCppBool)
+{
+    ASSERT_TRUE(script["boolt_var"]);
+    ASSERT_FALSE(script["boolf_var"]);
+}
+TEST_F(lua_value_Get, LuaStringAsCppString)
+{
+    std::string expected = "This is some string";
+    std::string actual=script["string_var"];
+    ASSERT_TRUE(expected == actual);
+}
+TEST_F(lua_value_Get,LuaStringAsCppChar)
+{
+    char expected='T';
+    char actual=script["string_var"];
+    ASSERT_TRUE(expected==actual);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppUnsignedLongLong)
+{
+    unsigned long long integer_var=script["integer_var"];
+    unsigned long long expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppLongLong)
+{
+    long long integer_var=script["integer_var"];
+    long long expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppUnsignedLong)
+{
+    unsigned long integer_var=script["integer_var"];
+    unsigned long expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppLong)
+{
+    long integer_var=script["integer_var"];
+    long expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get, LuaIntegerAsCppInt)
+{
+    int integer_var=script["integer_var"];
+    ASSERT_EQ(100,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppUnsignedInt)
+{
+    unsigned int integer_var=script["integer_var"];
+    unsigned int expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppUnsignedShort)
+{
+    unsigned short integer_var=script["integer_var"];
+    unsigned short expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get,LuaIntegerAsCppShort)
+{
+    short integer_var=script["integer_var"];
+    short expected=100;
+    ASSERT_EQ(expected,integer_var);
+}
+TEST_F(lua_value_Get, LuaNumberAsCppDouble)
+{
+    double double_var=script["double_var"];
+    ASSERT_DOUBLE_EQ(102.0351, double_var);
+}
+TEST_F(lua_value_Get, LuaNumberAsCppFloat)
+{
+    float double_var=script["double_var"];
+    ASSERT_FLOAT_EQ(102.0351f, double_var);
+}
---
+++ tests/core/lua_value_SetTest.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <string>
+#include <gtest/gtest.h>
+#include "core/lua_script.hpp"
+
+class lua_value_Set : public ::testing::Test
+{
+  public:
+    LuaBz::lua_script script;
+    void SetUp() override
+    {
+        std::string lua_script_file = "../tests/core/luascript_test.lua";
+        script.open(lua_script_file);
+    }
+};
+TEST_F(lua_value_Set, LuaVarFromCppBool)
+{
+    script["boolt_var"]=false;
+    ASSERT_FALSE(script["boolt_var"]);
+}
+TEST_F(lua_value_Set,LuaVarFromCppBoolExpression)
+{
+    script["boolt_var"]=(1==0);
+    ASSERT_FALSE(script["boolt_var"]);        
+}
+TEST_F(lua_value_Set, LuaVarFromCppString)
+{
+    std::string new_value = "This is the new value for the lua string";
+    script["string_var"]=new_value;
+    std::string actual=script["string_var"];
+    ASSERT_TRUE(new_value == actual);
+}
+TEST_F(lua_value_Set,LuaVarFromCppChar)
+{
+    char expected='Z';
+    script["string_var"]=expected;
+    char actual=script["string_var"];
+    ASSERT_TRUE(expected==actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppLongLong)
+{
+    long expected=666;
+    script["integer_var"]=expected;
+    long long actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppUnsignedLongLong)
+{
+    unsigned long long expected=666;
+    script["integer_var"]=expected;
+    unsigned long long actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+TEST_F(lua_value_Set, LuaVarFromCppLong)
+{
+    long expected=666;
+    script["integer_var"]=expected;
+    long actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppUnsignedLong)
+{
+    unsigned long expected=666;
+    script["integer_var"]=expected;
+    unsigned long actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+TEST_F(lua_value_Set, LuaVarFromCppInteger)
+{
+    int expected=666;
+    script["integer_var"]=expected;
+    int actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppUnsignedInteger)
+{
+    unsigned int expected=666;
+    script["integer_var"]=expected;
+    unsigned int actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppShort)
+{
+    short expected=666;
+    script["integer_var"]=expected;
+    short actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppUnsignedShort)
+{
+    unsigned short expected=666;
+    script["integer_var"]=expected;
+    unsigned short actual=script["integer_var"];
+    ASSERT_EQ(expected,actual);
+}
+TEST_F(lua_value_Set, LuaVarFromCppDouble)
+{
+    script["double_var"]=10.132265;
+    double double_var=script["double_var"];
+    ASSERT_DOUBLE_EQ(10.132265, double_var);
+}
+
+TEST_F(lua_value_Set, LuaVarFromCppFloat)
+{
+    float expected=10.132265f;
+    script["double_var"]=expected;
+    float actual=script["double_var"];
+    ASSERT_FLOAT_EQ(expected, actual);
+}
