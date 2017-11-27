096995e18cac0cf165fa6ea0f6ef98f6158b8727
Slavi <slavi.angelov94@gmail.com>
2017-11-27 21:41:04 +0100

    Removed comments from test file

--- tests/core/lua_script_Test.cpp
+++ tests/core/lua_script_Test.cpp
@@ -11,27 +11,6 @@ class lua_scriptF : public ::testing::Test
         script.open(lua_script_file);
     }
 };
-// TEST_F(lua_scriptF, LoadingDependencyAndMainScript)
-// {
-//     ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
-// }
-// TEST_F(lua_scriptF, LoadingMultipleDependenciesAndMainScript)
-// {
-//     ASSERT_TRUE(first_dependecy_string == script.get<string>("dependency"));
-//     ASSERT_TRUE(second_dependency_string == script.get<string>("dependency2"));
-// }
-// TEST_F(lua_scriptF, GetLuaTable)
-// {
-//     lua_table position = script.get<lua_table>("Position");
-//     int expected = 1;
-//     int actual = position["x"];
-//     ASSERT_EQ(expected, actual);
-// }
-///Test ideas
-///1 SetLuaIntegerWithX where x is any integral type
-///2 SetLuaNumberWithX where x is any integral type
-///3 SetLuaBoolWithX where x is anything that can be converted to bool
-
 TEST_F(lua_scriptF, ExecuteLuaCodeFromCppString)
 {
     std::string code = R"(variable=1;)"
