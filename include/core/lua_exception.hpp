61834add54dfcaf3224ba8dd5221630e38ea36b8
blazgrom <slavi.angelov94@gmail.com>
2017-11-29 21:58:19 +0100

    Added docs for lua_exception and made the c-tors for lua_exception explicit

--- include/core/lua_exception.hpp
+++ include/core/lua_exception.hpp
@@ -4,12 +4,20 @@
 #include <string>
 namespace LuaBz
 {
+/**
+ * \brief A class used to report any kind of error that lua can generate
+ *
+ * Class used to report all error that can be generated from lua.\n
+ * It's only used when the compiler flag USE_CPP_EXCEPTIONS is set to ON,
+ * the default value is off.
+ */
 class lua_exception : public std::runtime_error
 {
   public:
-    lua_exception(const std::string &msg);
-    lua_exception(const char *msg);
+    explicit lua_exception(const std::string &msg);
+    explicit lua_exception(const char *msg);
     const char *what() const noexcept override;
+
   private:
 };
 }
