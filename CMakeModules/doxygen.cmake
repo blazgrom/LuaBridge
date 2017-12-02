7b85bf3454a6b1b01ac59085a33ffd060fc69484
blazgrom <slavi.angelov94@gmail.com>
2017-12-02 16:04:30 +0100

    Removed the possibility to build docs on every build, now you have explicitly to call make doc

--- CMakeModules/doxygen.cmake
+++ CMakeModules/doxygen.cmake
@@ -5,19 +5,11 @@ find_package(Doxygen)
 if(DOXYGEN_FOUND)
     message(STATUS "Doxygen - documentation generator                YES ")
     configure_file(${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-    if(ALWAYS_BUILD_DOC)
-        add_custom_target(doc ALL
-        ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating API documentation with Doxygen" VERBATIM
-        )
-    else (ALWAYS_BUILD_DOC)
-        add_custom_target(doc
-        ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating API documentation with Doxygen" VERBATIM
-        )
-    endif(ALWAYS_BUILD_DOC)
+    add_custom_target(doc
+                      ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                      COMMENT "Generating API documentation with Doxygen" VERBATIM
+                    )
 else(DOXYGEN_FOUND)
     message(STATUS "Doxygen - documentation generator                NO ")
 endif(DOXYGEN_FOUND)
