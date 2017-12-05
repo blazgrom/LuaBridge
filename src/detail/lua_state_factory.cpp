5743530ced8b8e48bf4163b2ba699fab96b8c24a
blazgrom <slavi.angelov94@gmail.com>
2017-12-05 14:22:22 +0100

    active_lua_states and the master state are now visible only in the translation unit of lua_state_factory. Once a lua_State is retrieved from the master_state's registry table the value is popped from the master_state's stack in order to keep the stack clean, if this is not done it can cause seg fault when the master_state's stack becomes full

--- src/detail/lua_state_factory.cpp
+++ src/detail/lua_state_factory.cpp
@@ -1,11 +1,12 @@
 #include "detail/lua_state_factory.hpp"
-#include "detail/lua_error.hpp"
 #include <unordered_map>
-#include <iostream>
+#include "detail/lua_error.hpp"
 namespace luabz
 {
 namespace detail
 {
+static std::unordered_map<std::string, int> active_lua_states;
+static lua_State* master_state = luaL_newstate();
 /**
  * \todo \n
  * 1-When the file has already been loaded into a lua thread we should \n
@@ -13,21 +14,25 @@ namespace detail
  *
  * \todo Implement load of lua std
  */
+
 lua_State* lua_state_factory::create_state(const std::string& file_name,
                                            bool load_std)
 {
-    static   std::unordered_map<std::string, int> active_lua_states;
-    static lua_State* master_state = luaL_newstate();
-    std::string registry_key = "luabz_" + file_name;
-    if (active_lua_states.count(registry_key)) {
-        auto lua_ref=active_lua_states[registry_key];
-        lua_rawgeti(master_state, LUA_REGISTRYINDEX,
-                    lua_ref);
-        return lua_tothread(master_state, -1);
+    bool already_inserted=active_lua_states.find(file_name) != active_lua_states.end();
+    if (already_inserted) {
+        auto lua_ref = active_lua_states[file_name];
+        lua_rawgeti(master_state, LUA_REGISTRYINDEX, lua_ref);
+        if(lua_isthread(master_state,-1))
+        {
+            lua_State* state =lua_tothread(master_state,-1);
+            lua_pop(master_state,-1);
+            return state;
+        }
+        lua_error("Lua slave state not found");
     }
     lua_State* state = lua_newthread(master_state);
     auto lua_ref = luaL_ref(master_state, LUA_REGISTRYINDEX);
-    active_lua_states.emplace(registry_key,lua_ref);
+    active_lua_states.emplace(file_name, lua_ref);
     set_state_globaltable(state);
     if (luaL_dofile(state, file_name.c_str())) {
         lua_error(lua_tostring(state, -1));
