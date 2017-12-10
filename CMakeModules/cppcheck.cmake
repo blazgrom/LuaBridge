bfdaf8c9651998316163a51fa187b3b4b198fe7a
blazgrom <slavi.angelov94@gmail.com>
2017-12-10 21:08:12 +0100

    Updated the cppcheck cmake module

--- CMakeModules/cppcheck.cmake
+++ CMakeModules/cppcheck.cmake
@@ -1,20 +1,30 @@
 find_program(CPPCHECK_PATH cppcheck)
 if(CPPCHECK_PATH)
-    message(STATUS "cppcheck - static analysis                YES ")
+    message(STATUS "cppcheck - static analysis                          YES ")
     list(APPEND CPPCHECK_ARGS
-    --enable=warning,style,performance,portability,unusedFunction
+    --enable=all
     --std=c++11
-    --verbose
     --language=c++
+    --xml-version=2
+    --force
+    --suppress=missingIncludeSystem
+    --library=windows,posix,gnu
     -I ${CMAKE_SOURCE_DIR}/include
     ${CMAKE_SOURCE_DIR}/include/*/*.h
+    ${CMAKE_SOURCE_DIR}/include/*/*.hpp    
     ${CMAKE_SOURCE_DIR}/src/*.cpp
-    ${CMAKE_SOURCE_DIR}/src/*/*.cpp)
-
+    ${CMAKE_SOURCE_DIR}/src/*/*.cpp
+    2> result.xml &&
+    cppcheck-htmlreport
+    --source-encoding="iso8859-1"
+    --title=${PROJECT_NAME}
+    --source-dir= ${CMAKE_SOURCE_DIR}/include/* ${CMAKE_SOURCE_DIR}/src/*
+    --report-dir=${REPORTS_OUTPUT_PATH}/cppcheck
+    --file=result.xml && rm result.xml)
     add_custom_target(
             cppcheck
-            COMMAND ${CPPCHECK_PATH} ${CPPCHECK_ARGS} 
-            COMMENT "running cppcheck" )
+            ${CPPCHECK_PATH} ${CPPCHECK_ARGS} 
+            COMMENT "Running cppcheck and generating report")
 else()
-    message(STATUS "cppcheck - static analysis                NO")
+    message(STATUS "cppcheck - static analysis                              NO")
 endif()
