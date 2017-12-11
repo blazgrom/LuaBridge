389300a1393a89a5194098f97a7e166f38060079
blazgrom <slavi.angelov94@gmail.com>
2017-12-11 22:09:14 +0100

    The path to the folder containing the lua script is now constructed with the help of a macro defined in the primary CMakeLists.txt

--- tests/core/lua_test_helpers.hpp
+++ tests/core/lua_test_helpers.hpp
@@ -1,5 +1,6 @@
 #ifndef TEST_HELPERS_HPP
 #define TEST_HELPERS_HPP
+#include <cstdio>
 #include <string>
 //TOOD (blazgrom): Move this file to detail,and delete the folder core. The test folder structure should be identical to the structure of the include/src
 inline std::string construct_script_path(std::string script_name)
@@ -11,7 +12,8 @@ inline std::string construct_script_path(std::string script_name)
         script_name += lua_extension;
     }
     //TODO (blazgrom) : find a better way of defining the base folder
-    const std::string script_folder = "../tests/lua_scripts/";
-    return script_folder + script_name;
+    auto project_folder=LUABZ_PROJECT_FOLDER_PATH;
+    const std::string script_folder = "/tests/lua_scripts/";
+    return project_folder+script_folder + script_name;
 }
 #endif
