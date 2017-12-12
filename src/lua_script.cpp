eb13d38c45b01bfaefce28007029823a03499b29
blazgrom <slavi.angelov94@gmail.com>
2017-12-12 22:21:00 +0100

    Added documentation for lua_script

--- src/lua_script.cpp
+++ src/lua_script.cpp
@@ -1,47 +1,25 @@
 #include "lua_script.hpp"
 #include <algorithm>
+#include "detail/lua_error.hpp"
 #include "detail/lua_state_factory.hpp"
 #include "lua_exception.hpp"
 namespace luabz
 {
-// C-tors
 // TODO :
 // Implement all empty functions
 lua_script::lua_script() : m_state{nullptr}, m_fileName{""}, m_open{false} {}
-lua_script::lua_script(const std::string& file, bool loadStandardLib)
-  : m_state{detail::lua_state_factory::create_state(file, loadStandardLib)},
+lua_script::lua_script(const std::string& file, bool lua_stl)
+  : m_state{detail::lua_state_factory::create_state(file, lua_stl)},
     m_fileName{file},
     m_open{true}
 {
 }
-lua_script::lua_script(const std::string& file,
-                       const std::vector<std::string>& dependencies,
-                       bool loadStandardLib)
-  : m_state{detail::lua_state_factory::create_state(file, loadStandardLib)},
-    m_fileName{file},
-    m_open{true}
-
-{
-}
-lua_script::lua_script(const std::string& file,
-                       const std::string& dependency,
-                       bool loadStandardLib)
-  : m_state{detail::lua_state_factory::create_state(file, loadStandardLib)},
-    m_fileName{file},
-    m_open{true}
-
-{
-}
-
-void lua_script::open(const std::string& file,
-                      const std::vector<std::string>& dependencies,
-                      bool loadStandardLib)
+void lua_script::open(const std::string& file_name, bool lua_stl)
 {
     if (!m_open) {
-        m_state =
-            detail::lua_state_factory::create_state(file, loadStandardLib);
+        m_state = detail::lua_state_factory::create_state(file_name, lua_stl);
         m_open = true;
-        m_fileName = file;
+        m_fileName = file_name;
     }
 }
 void lua_script::close() noexcept
@@ -49,31 +27,18 @@ void lua_script::close() noexcept
     // m_stack.destroy();
     // m_open = false;
 }
-bool lua_script::change(const std::string& newFile) noexcept
+void lua_script::change(const std::string& file_name,
+                        bool lua_stl) noexcept
 {
-    bool result = true;
-    // try {
-    //     m_stack.destroy();
-    //     m_stack.create(newFile);
-    //     m_fileName = newFile;
-    // } catch (...) {
-    //     m_stack.destroy();
-    //     m_stack.create(m_fileName);
-    //     result = false;
-    // }
-    return result;
+    m_state = detail::lua_state_factory::create_state(file_name, lua_stl);
+    m_open = true;
+    m_fileName = file_name;
 }
-/**
- * \todo topElement should not be defined here \n
- * Fine a better way  to communicate that a lua error has occured
- *
- * \todo Use lua_error here
- */
 void lua_script::operator()(const std::string& lua_code) const
 {
     static const int top = -1;
     if (luaL_dostring(m_state, lua_code.c_str())) {
-        throw lua_exception(lua_tostring(m_state, top));
+        detail::lua_error(lua_tostring(m_state, top));
     }
 }
 lua_value_ref lua_script::operator[](const std::string& name) const
