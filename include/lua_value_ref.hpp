78c87d3085abdb91817ca8c21ca0f3812d609425
blazgrom <slavi.angelov94@gmail.com>
2017-12-20 22:45:09 +0100

    WIP: Logic for assignment of std::function to a lua_value_ref and logic for calling the assigned function

--- include/lua_value_ref.hpp
+++ include/lua_value_ref.hpp
@@ -1,9 +1,18 @@
 #ifndef LUABZ_LUA_VAL_EXPR_HPP
 #define LUABZ_LUA_VAL_EXPR_HPP
 #include <cstddef>
+#include <functional>
 #include <lua.hpp>
 #include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+#include "detail/lua_error.hpp"
 #include "detail/lua_value.hpp"
+/**
+ * \todo This include should not used relative path, but just Utils/variadic_index
+*/
+#include "../Utils/variadic_index.hpp"
 namespace luabz
 {
 class lua_script;
@@ -16,6 +25,7 @@ class lua_value_ref
                                      ///< because we want the only way to create
                                      ///< lua_value_ref to be through a
                                      ///< lua_script
+    static std::vector<std::function<int(lua_State*)>> registeredFunctions;
 
   public:
     lua_value_ref(const lua_value_ref& rhs);
@@ -53,6 +63,96 @@ class lua_value_ref
         clear_used_stack_spaces();
         return *this;
     }
+    // WIP
+    template <class ReturnType, class... Args>
+    lua_value_ref& operator=(std::function<ReturnType(Args...)>& f)
+    {
+        // WARNING: Not thread safe, we are accessing a static variable
+        registeredFunctions.emplace_back([this, f](lua_State*) -> int {
+            // Invoke the function only if we have the correct number of
+            // arguments on the stack
+            if (lua_gettop(m_state) == sizeof...(Args)) {
+                std::tuple<ReturnType,Args...> t{};
+                return call_registered_function(f,t);
+            }
+            return 0;
+        });
+        register_function();
+        return *this;
+    }
+    // WIP
+    template <typename... Args>
+    lua_value_ref operator()(Args&&... args)
+    {
+        load_lua_var();
+        insert_parameters(std::forward<Args>(args)...);
+        const int input_count = sizeof...(Args);
+        const int output_count = 1;
+        if (lua_pcall(m_state, input_count, output_count, 0) != 0) {
+            detail::lua_error(lua_tostring(m_state, -1));
+        }
+        auto return_value_name=m_name+"_return_value";
+        lua_setfield(m_state, LUA_GLOBALSINDEX, return_value_name.c_str());
+        clear_used_stack_spaces();
+        return lua_value_ref(m_state,return_value_name);
+    }
+    // WIP
+    //Note: values pushed though this function are never popped, fix it
+    template <class T, class... Args>
+    void insert_parameters(T&& value, Args&&... args)
+    {
+        detail::lua_value<T>::insert(m_state, value);
+        insert_parameters(args...);
+    }
+    // WIP
+    template <class T>
+    void insert_parameters(T&& value)
+    {
+        detail::lua_value<T>::insert(m_state, value);
+    }
+    void insert_parameters()
+    {
+        return;
+    }
+    // WIP
+    void register_function()
+    {
+        // Note:
+        // The index at which the function is saved in the static C++ vector
+        // becomes an upvalue for the function
+        load_lua_var();
+        std::size_t position_index = registeredFunctions.size() - 1;
+        lua_pushinteger(m_state, position_index);
+        lua_CFunction new_value = [](lua_State* functionState) -> int {
+            auto functionIndex = lua_tointeger(
+                functionState, lua_upvalueindex(1));  // retrieve  upvalue
+            auto& function = registeredFunctions.at(functionIndex);
+            return function(functionState);
+        };
+        unsigned int upvalues_count = 1;
+        detail::lua_value<lua_CFunction>::insert(m_state, new_value,
+                                                 upvalues_count);
+        set_lua_var();
+        clear_used_stack_spaces();
+    }
+    // WIP
+    template <class T, class R, class... Args>
+    int call_registered_function(T& user_f,std::tuple<R,Args...>)
+    {
+        // TODO: Find solution for the following problem
+        // auto result =
+        // user_f(m_stack.get_element<Args>((index_generator.get_index()
+        // + 1)*-1)...);  This solution works under Microsoft  compiler and GCC
+        // compiler
+        Utils::variadric_index<Args...> index_generator;
+        //This should call detail::lua_value<Args> with an index 
+        auto result = user_f(m_stack.get_element<Args>(
+             (index_generator.get_index() + 1) * -1)...);
+        lua_pop(m_state, sizeof...(Args));
+        detail::lua_value<decltype(result)>::insert(m_state,result);
+        return 1;
+    }
+
     /**
      * \brief Operator= between lua_value_ref and any type T
      * \note Temporary variable of type T is created, this temporal represents
