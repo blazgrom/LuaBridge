179099d38c50de8516989b500925cf2a797db033
blazgrom <slavi.angelov94@gmail.com>
2018-01-13 12:02:29 +0100

    Lua threads are no longer used inside lua_state_factory. lua_state_factory now creates a full blown lua_State for every file.

--- include/detail/lua_state_factory.hpp
+++ include/detail/lua_state_factory.hpp
@@ -13,19 +13,9 @@ namespace detail
  * \brief A factory used to generate/retrieve new lua_State*
  *
  * The generation of new lua states uses the following logic.
- * We have one master state which is unique for whole application, whenever the
- * user wants to open a new lua file, we create a new thread (lua_State) on the
- * master state and insert it in the master state's registry in order to
- * guarantee that our state want be collected by Lua's garbage collector. Before
- * returning the new thread (lua_State) do the caller we map the state to the
- * name of the lua file. This is done for the following reasons:\n
- * 1 - We can have two differnt lua_scripts that operate on the same lua
- * file,sharing data through lua.\n 2 - We don't have to lookup the state in the
- * master state's registry whenever we can two consecutive calls for the same
- * file\n If a lua file has already been open, the old lua state associated with
- * the file is returned \todo Decide if we want to delete some of the thread
- * inside the master state's registry after a while, and decide the deletion
- * should be performed\n
+ * For each lua file we want to open we create a new lua state. The state
+ * is closed only when explicitly required by the lua_script object, this permits us to 
+ * share the same lua file between multiple lua_script object.
  */
 class lua_state_factory
 {
@@ -33,14 +23,11 @@ class lua_state_factory
     /// Generates new lua_state to be used by lua_script
     /**
      *
-     * \note Every new lua_thread is pushed onto the registry in order \n
-     * to not allow GC from Lua
      * \param file_name The name of the lua file on which the new lua state
      * \param load_std Whether or not to load lua's std
      * operates
-     * \note The first type this function is called with file_name that has not
-     * yet been used, the file is loaded. All subsequent calls use the already
-     * loaded filestatic
+     * \note If there is an already opened lua state associated with file_name thats the one thats
+     * returned, otherwise a new lua state is created
      */
     static lua_State* get_lua_state(const std::string& file_name,
                                     bool load_std = false);
@@ -49,26 +36,25 @@ class lua_state_factory
      * \param file_name The name of the lua file
      * \pre There must be an already opened lua state asssociated with the
      * file_name
+     * \param file_name The name of the lua file inside which we want to open the standard library
      */
     static void open_standard_library(const std::string& file_name);
+    /**
+     * \brief Closes the lua_State associated with file_name, thus destroying all object in
+     * the givedn lua_State and frees all dynamica memory used by the state
+     * \pre There must be an already opened lua state asscociated with the file_name
+     * \param file_name The name of the file for which we want to close the lua state
+    */
+    static void close_lua_state(const std::string& file_name);
 
   private:
     /**
-     * \brief Sets the global table of the new state
-     * \param state The state for which we want to change the globaltable
-     * \note Has the following logic \n
-     * 1 ) Insert the new global table and the metatable of the former \n
-     * 2 ) Replace the current global table with the new global table
-     */
-    static void set_state_globaltable(lua_State* state);
-    /**
      * \brief Retrieves the lua script associated with file_name from
-     * master_state's registry
+     * from the collection of opened lua states
      */
     static lua_State* get_loaded_lua_state(const std::string& file_name);
     /**
      * \brief Creates new lua_State which is associated with the script file
-     * file_name and inserts the lua_Sate in active_lua_states
      */
     static lua_State* create_new_lua_state(const std::string& file_name,
                                            bool load_std);
--- src/detail/lua_state_factory.cpp
+++ src/detail/lua_state_factory.cpp
@@ -5,7 +5,7 @@ namespace luabz
 {
 namespace detail
 {
-static std::unordered_map<std::string, int> active_lua_states;  ///< Contains
+static std::unordered_map<std::string, lua_State*> active_lua_states;  ///< Contains
                                                                 ///< all the lua
                                                                 ///< states
                                                                 ///< connected
@@ -13,39 +13,26 @@ static std::unordered_map<std::string, int> active_lua_states;  ///< Contains
                                                                 ///< already
                                                                 ///< open lua
                                                                 ///< file
-static lua_State* master_state =
-    luaL_newstate();  ///< The master lua state, which repository is used to
-                      ///< store the other lua states. This state is necessary
-                      ///< because it's the only one that lua will not
-                      ///< automatically collect, so it's used to store the
-                      ///< other lua states
-
 lua_State* lua_state_factory::get_loaded_lua_state(const std::string& file_name)
 {
-    auto lua_ref = active_lua_states[file_name];
-    lua_rawgeti(master_state, LUA_REGISTRYINDEX, lua_ref);
-    if (lua_isthread(master_state, -1)) {
-        lua_State* state = lua_tothread(master_state, -1);
-        lua_pop(master_state, -1);
-        return state;
-    }
-    lua_error("Lua slave state not found");
-    return nullptr;
+    return active_lua_states[file_name];
 }
 
 lua_State* lua_state_factory::create_new_lua_state(const std::string& file_name,
                                                    bool load_std)
 {
-    lua_State* state = lua_newthread(master_state);
-    auto lua_ref = luaL_ref(master_state, LUA_REGISTRYINDEX);
-    active_lua_states.emplace(file_name, lua_ref);
-    set_state_globaltable(state);
+    lua_State* state = luaL_newstate();
+    if(state==NULL)
+    {
+        lua_error("Cannot create a new lua_State");
+    }
     if (luaL_dofile(state, file_name.c_str())) {
         lua_error(lua_tostring(state, -1));
     }
     if (load_std) {
         luaL_openlibs(state);
     }
+    active_lua_states[file_name]=state;
     return state;
 }
 lua_State* lua_state_factory::get_lua_state(const std::string& file_name,
@@ -63,13 +50,12 @@ void lua_state_factory::open_standard_library(const std::string& file_name)
     lua_State* m_state = get_loaded_lua_state(file_name);
     luaL_openlibs(m_state);
 }
-
-void lua_state_factory::set_state_globaltable(lua_State* state)
+void lua_state_factory::close_lua_state(const std::string& file_name)
 {
-    // Insert the new global table
-    lua_newtable(state);
-    // Set the new global table
-    lua_replace(state, LUA_GLOBALSINDEX);
+    lua_State* state=get_loaded_lua_state(file_name);
+    lua_close(state);
+    active_lua_states.erase(file_name);
 }
+
 }  // namespace detail
 }  // namespace luabz
