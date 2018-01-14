e055ba331dace057f25e02637a688ab6f5d61ce0
blazgrom <slavi.angelov94@gmail.com>
2018-01-14 11:52:07 +0100

    Running all tests

--- run_build.sh
+++ run_build.sh
@@ -37,14 +37,14 @@ if [ $? -ne 0 ]; then
     exit 3
 fi
 cd ../bin
-run_tests lua_scriptF
-run_tests lua_value_Get
-run_tests lua_value_Operator
-run_tests lua_value_Set
-run_tests lua_value_TableTest
-run_tests lua_value_Test
-run_tests lua_value_Function
-
+# run_tests lua_scriptF
+# run_tests lua_value_Get
+# run_tests lua_value_Operator
+# run_tests lua_value_Set
+# run_tests lua_value_TableTest
+# run_tests lua_value_Test
+# run_tests lua_value_Function
+./luabz_tests
 
 #Uncomment if you want to run cppcheck
 #workingprocess "Running cppcheck"
